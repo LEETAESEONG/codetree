aace5b071295e75507f7f48668d8c9fe7523d81e
MuscleTae <122497473+LEETAESEONG@users.noreply.github.com>
2025-01-16 14:08:17 +0900

    Title: 원소의 합이 0, Time: 207ms, Memory: 43MB, Status: Passed - CodeTree

--- 250116/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 250116/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -6,16 +6,8 @@ D = list(map(int, input().split()))
 
 # Write your code here!
 answer = 0
-# AB AC AD
-# CD BD BC
 dictAB = dict()
 dictCD = dict()
-
-# dictAC = dict()
-# dictBD = dict()
-
-# dictAD = dict()
-# dictBC = dict()
 
 def addKey(dictTmp, left, right):
     for i in range(n):
@@ -31,28 +23,9 @@ def addKey(dictTmp, left, right):
 addKey(dictAB, A, B)
 addKey(dictCD, C, D)
 keysAB = list(dictAB.keys())
-keysCD = list(dictCD.keys())
 for kab in keysAB:
-    for kcd in keysCD:
-        if kab + kcd == 0:
-            answer += dictAB[kab] * dictCD[kcd]
-
-# addKey(dictAC, A, C)
-# addKey(dictBD, B, D)
-# keysAC = list(dictAC.keys())
-# keysBD = list(dictBD.keys())
-# for kac in keysAC:
-#     for kbd in keysBD:
-#         if kac + kbd == 0:
-#             answer += dictAC[kac] * dictBD[kbd]
-
-# addKey(dictAD, A, D)
-# addKey(dictBC, B, C)
-# keysAD = list(dictAD.keys())
-# keysBC = list(dictBC.keys())
-# for kad in dictAD:
-#     for kbc in dictBC:
-#         if kad + kbc == 0:
-#             answer += dictAD[kad] * dictBC[kbc]
+    kcd = 0 - kab
+    if dictCD.get(kcd, 0):
+        answer += dictAB[kab] * dictCD[kcd]
 
 print(answer)
