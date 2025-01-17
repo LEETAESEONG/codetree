4bfaa673990ab0d2a3741f92f4226924f5e98dd0
MuscleTae <122497473+LEETAESEONG@users.noreply.github.com>
2025-01-17 16:52:09 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 99ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 250117/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
@@ -0,0 +1,22 @@
+n = int(input())
+words = [input() for _ in range(n)]
+
+# Write your code here!
+asciiDict = dict()
+def makeKeys(word):
+    tmpArr = [0] * 52 # 앞 26개는 대문자 뒤 26개는 소문자
+    for w in word:
+        ascii = ord(w)
+        if ascii < 97:
+            tmpArr[ascii-65] += 1
+        else:
+            tmpArr[ascii-71] += 1
+    return "".join(map(str, tmpArr))
+for word in words:
+    asciiKey = makeKeys(word)
+    if asciiKey in asciiDict:
+        asciiDict[asciiKey] += 1
+    else:
+        asciiDict[asciiKey] = 1
+maxCnt = max(list(asciiDict.values()))
+print(maxCnt)
